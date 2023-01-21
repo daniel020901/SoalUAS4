6a56548dcb123d8c5bac49638d8e8bc4f49f46a8
muhammad daniel ilyasa <115854547+daniel020901@users.noreply.github.com>
2023-01-21 10:50:20 +0700

    Add files via upload

---
+++ SoalUAS4.java
@@ -0,0 +1,27 @@
+package com.tutorial;
+
+public class SoalUAS4 {
+
+	public static void main(String[] args) {
+        
+		int n = 5;
+		int i, j;
+		
+				for (i=0; i<=4; i++) { 
+					     
+					     for(j=0; j<=i; j++) 
+					     {
+					    	 System.out.print(j);
+					    	 
+					     }
+					      System.out.println();
+				}
+			
+		}
+}
+			
+		
+	
+
+
+
